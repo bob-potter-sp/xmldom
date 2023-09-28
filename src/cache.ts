0be5025f77ab2fd82ac0f9a40bc0346c8a77ece6
Bob Potter <bob.potter@sailpoint.com>
2023-09-28 10:30:46 -0500

    diffy

--- src/cache.ts
+++ src/cache.ts
@@ -1,4 +1,4 @@
-var stringCache: Map<string,string> = new Map();
+var stringCache: Record<string,string>;
 var stringCount = 0;
 
 export function intern(s: string): string {
@@ -6,9 +6,17 @@ export function intern(s: string): string {
   if (stringCount % 100_000 == 0) {
     console.log(`string hits ${stringCount}`);
   }
+  if (!stringCache[s]) {
+    stringCache[s] = s;
+    console.log(`stringCache ${stringCache.size}`);
+  }
+  return stringCache[s];
+  /*
+  return stringCache.get(s)!;
   if (!stringCache.has(s)) {
     stringCache.set(s, s);
     console.log(`stringCache ${stringCache.size}`);
   }
   return stringCache.get(s)!;
+ */
 }
